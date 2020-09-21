8093427b0ff74c9571abe0385edc146c065e9243
pankourgiou <60981745+pankourgiou@users.noreply.github.com>
2020-09-21 12:17:57 +0300

    Create Reducesmoking.py

---
+++ Reducesmoking.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#x = 1=0
+
+x = "1+1=2+1=3+1=4=0"
+x1 = "NIN = Nine inch Nails"
+x2 = "Distance over time = S2N = WOW = False"
+x3 = "I am a copy of a copy"
+x4 = "-*+*-=0"
+x5 = "-*-*-=0"
+x6 = "we are on ground 0"
+smoking = "False"
+smoking1 = smoking
+yes = "Reduce smoking"
+
+
+print("False")
+print(bool(x1))
+print("False")
+print(bool(x3))
+print(bool(x4))
+print(bool(x5))
+print(bool(x6))
+print(bool(smoking))
+print(bool(smoking1))
+print("Reduce smoking\n"*10)
