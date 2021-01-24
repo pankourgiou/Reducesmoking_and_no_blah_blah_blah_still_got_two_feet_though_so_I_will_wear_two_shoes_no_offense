a26806db48a06f3c69a1561f6b847976992920d1
pankourgiou <60981745+pankourgiou@users.noreply.github.com>
2021-01-24 21:09:23 +0200

    Update Reducesmoking.py

--- Reducesmoking.py
+++ Reducesmoking.py
@@ -2,9 +2,8 @@
 # -*- coding: utf-8 -*-
 #x = 1=0
 
-x = "1+1=2+1=3+1=4=0"
+
 x1 = "NIN = Nine inch Nails"
-x2 = "Distance over time = S2N = WOW = False"
 x3 = "I am a copy of a copy"
 x4 = "-+-=0"
 x5 = "-*-*-=0"
@@ -14,9 +13,8 @@ smoking1 = smoking
 yes = "Reduce smoking"
 
 
-print("False")
+
 print(bool(x1))
-print("False")
 print(bool(x3))
 print(bool(x4))
 print(bool(x5))
